8825b8f257c5de5b95187633282a285ceaa272bc
collinskirui <collins.kirui@strathmore.edu>
2023-02-05 23:49:46 +0300

    install firebase and react-toastify and sign up the user

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAesFWkgFg9rBmMAyg1fhCBOCxzVzeq55k",
+  authDomain: "realtor-clone-react-50c1d.firebaseapp.com",
+  projectId: "realtor-clone-react-50c1d",
+  storageBucket: "realtor-clone-react-50c1d.appspot.com",
+  messagingSenderId: "996635975993",
+  appId: "1:996635975993:web:c769244a6550899e6508e8"
+};
+
+// Initialize Firebase
+ initializeApp(firebaseConfig);
+ export const db = getFirestore()
