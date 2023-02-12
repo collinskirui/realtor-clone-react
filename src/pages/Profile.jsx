b4efe4b75a8b7eae1143ebf6ef8cbf4490248bb3
collinskirui <collins.kirui@strathmore.edu>
2023-02-12 16:31:18 +0300

    Create the create-listing page UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,11 @@
 import { getAuth, updateProfile } from 'firebase/auth';
 
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
 import {toast} from "react-toastify"
 import { doc, updateDoc } from 'firebase/firestore';
-
+import {FcHome} from "react-icons/fc"
 
 
 export default function Profile() {
@@ -75,8 +75,14 @@ export default function Profile() {
         </p>
         <p onClick={onLogout} className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer">Sign Out</p>
       </div>
-
       </form>
+      <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800" >
+        <Link to= "/create-listing" className="flex justify-center items-center ">
+        <FcHome className="mr-2 text-3xl bg-red-200 rounded-full p-1 border-2" />
+        Sell or rent your home
+        </Link>
+       
+      </button>
 
       </div>
     </section>
